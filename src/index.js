9f931f14c1a0eace68f4bf032482e8af3be820e0
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-03 20:03:52 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,67 +1,61 @@
-let breeds = [];
+console.log('%c HI', 'color: firebrick')
+//variables
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+let ul = document.getElementById("dog-breeds");
 
-document.addEventListener('DOMContentLoaded', function () {
-    loadImages();
-    loadBreedOptions();
-});
-
-
-function loadImages() {
-    const imgUrl ="https://dog.ceo/api/breeds/image/random/4"
+//functions
+function fetchImg(){
 return fetch(imgUrl)
 .then(res => res.json())
-.then(rsults => {
-    results.message.forEach(image => addImage(image))
+.then(results =>{ 
+    results.message.forEach(image => addImg(image));
 });
 }
 
-function addImage(dogPicUrl) {
-    let container = document.querySelector('#dog-image-container');
+function addImg(dogImgUrl){
+    let container = document.querySelector("#dog-image-container");
     let newImageEl = document.createElement('img');
-    newImageEl.src = dogPicUrl;
+    newImageEl.src = dogImgUrl;
     container.appendChild(newImageEl);
 }
 
-function loadBreedOptions() {
-    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-    fetch(breedUrl)
-    .then(res => res.json())
-    .then(results => {
-
+function loadBreedOptions(){
+fetch(breedUrl)
+.then(res => res.json())
+.then(results =>{ 
         breeds = Object.keys(results.message);
         updateBreedList(breeds);
         addBreedSelectListener();
     });
 }
 
-function updateBreedList(breeds) {
+function updateBreedList(breeds){
     let ul = document.querySelector('#dog-breeds');
     removeChildren(ul);
     breeds.forEach(breed => addBreed(breed));
 }
-
 function removeChildren(element){
     let child = element.lastElementChild;
-    while(child) {
+    while(child){
         element.removeChild(child);
         child = element.lastElementChild;
     }
 }
 
-function selectBreedsStartigWith(letter) {
+function selectBreedsStartingWith(letter){
     updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
 }
 
-function addBreedSelectListener() {
-    let breedDropdow = document.querySelector('#breed-dropdown');
-    breedDropdown.addEventListener('change', function(event) {
+function addBreedSelectListener(){
+    let breedDropDown = document.querySelector('#breed-dropdown');
+    breedDropDown.addEventListener('change', function(event) {
         selectBreedsStartingWith(event.target.value);
     });
 }
 
-
-function addBreed(breed) {
-    let ul = document.querySelector('#dogs-breeds');
+function addBreed(breed){
+    let ul = document.querySelector('#dog-breeds');
     let li = document.createElement('li');
     li.innerText = breed;
     li.style.cursor = 'pointer';
@@ -69,6 +63,24 @@ function addBreed(breed) {
     li.addEventListener('click', updateColor);
 }
 
-function updateColor(event) {
-    event.target.style.color = 'palevioletred';
+function updateColor(event){
+    event.target.style.color = 'rgb(50,200,20)';
 }
+    
+   
+
+document.addEventListener("DOMContentLoaded", function () {
+ fetchImg();
+ loadBreedOptions();
+})
+
+
+//user story
+//fetch the images using the url above [x]
+//parse the response as JSON   [X]
+//add image elements to the DOM for each🤔 image in the array [X]
+//fetch all the dog breeds using the url above [X]
+//add the breeds to the page in an <ul> [X]
+// Once all of the breeds are rendered in the <ul>, add JavaScript so that the font color of a particular <li> changes on click. This can be a color of your choosing.
+//When the user clicks any of the dog breed list items, the color the text should change.[x]
+//Once we are able to load all of the dog breeds onto the page, add JavaScript so that the user can filter breeds that start with a particular letter using a dropdown.[x]
